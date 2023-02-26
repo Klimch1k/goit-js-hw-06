cd1ce1a728b5ee2ba3fcff5b43578c59ce6fcb4f
Klimch1k <114351322+Klimch1k@users.noreply.github.com>
2023-02-26 15:24:07 +0200

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const ingredientsItems = ingredients.map((ingredient) => {
+  const itemLi = document.createElement("li");
+  itemLi.textContent = ingredient;
+  itemLi.classList.add("item");
+  return itemLi;
+});
+ingredientsList.append(...ingredientsItems);
